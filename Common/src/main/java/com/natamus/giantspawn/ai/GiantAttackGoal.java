88ecfc9cb3b7492a290f5766270430ef34132125
Rick South <rick@natamus.com>
2026-04-02 16:10:18 +0200

    Giant Spawn source code for 26.1.1.

---
+++ Common/src/main/java/com/natamus/giantspawn/ai/GiantAttackGoal.java
@@ -0,0 +1,45 @@
+package com.natamus.giantspawn.ai;
+
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.monster.Giant;
+
+public class GiantAttackGoal extends MeleeAttackGoal {
+	   private int ticksUntilNextAttack; // attackTick
+	   private final Giant giant;
+	   private int raiseArmTicks;
+
+	   public GiantAttackGoal(Giant giantIn, double speedIn, boolean longMemoryIn) {
+		  super(giantIn, speedIn, longMemoryIn);
+		  this.giant = giantIn;
+	   }
+
+	   /**
+		* Execute a one shot task or start executing a continuous task
+		*/
+	   public void start() {
+		  super.start();
+		  this.raiseArmTicks = 0;
+	   }
+
+	   /**
+		* Reset the task's internal state. Called when this task is interrupted by another one
+		*/
+	   public void stop() {
+		  super.stop();
+		  this.giant.setAggressive(false);
+	   }
+
+	   /**
+		* Keep ticking a continuous task that has already been started
+		*/
+	   public void tick() {
+		  super.tick();
+		  ++this.raiseArmTicks;
+		  if (this.raiseArmTicks >= 5 && this.ticksUntilNextAttack < 10) {  // attackTick
+			 this.giant.setAggressive(true);
+		  } else {
+			 this.giant.setAggressive(false);
+		  }
+
+	   }
+}
